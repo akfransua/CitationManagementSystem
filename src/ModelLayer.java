d4c818d61c7180c976275eb7ccde731d1d715ddc
francis <francis@francis-ThinkPad-T420>
2018-02-20 15:43:45 -0500

    Update version of addCitationWidow Signed-off-by: francis <francis@francis-ThinkPad-T420>

--- src/ModelLayer.java
+++ src/ModelLayer.java
@@ -5,24 +5,31 @@ import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class ModelLayer {
-	private Connection connect = null;
+	private static Connection connect = null;
     private Statement statement = null;
     private PreparedStatement preparedStatement = null;
     private ResultSet resultSet = null;
     
+    /**
+     * 
+     * @param user_name
+     * @param user_pwd
+     * @param db_name
+     * @throws Exception
+     */
 	
-    void connectDB() throws Exception {
+    
+    void connectDB(String user_name, String user_pwd, String db_name) throws Exception {
     	  // This will load the MySQL driver, each DB has its own driver
         Class.forName("com.mysql.jdbc.Driver");
         // Setup the connection with the DB
-        connect = DriverManager
-                .getConnection("jdbc:mysql://localhost/feedback?"
-                        + "user=sqluser&password=sqluserpw");
+     //   connect = DriverManager.getConnection("jdbc:mysql://localhost/feedback", "sqluser","sqluserpw");
+        connect = DriverManager.getConnection("jdbc:mysql://localhost/"+db_name, user_name, user_pwd);
 
 	}
     
-    public Connection getConnectionObj() {
-    	return connect;
+    public static Connection getConnectionObj() {
+    	return ModelLayer.connect;
     }
 
 }
